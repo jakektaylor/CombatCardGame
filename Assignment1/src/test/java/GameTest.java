5ec73482d389a62eda73044b58acd8d1219b7be3
Jake Taylor <jakektaylor@cmail.carleton.ca>
2023-09-30 01:00:59 -0400

    U-TEST 002: Testing invalid user input when entering the number of players.

--- Assignment1/src/test/java/GameTest.java
+++ Assignment1/src/test/java/GameTest.java
@@ -17,4 +17,18 @@ class GameTest {
         game.setNumPlayers(new Scanner("3\n"), new PrintWriter(output));
         assertEquals(3, game.getNumPlayers());
     }
+
+    @Test
+    @DisplayName("U-TEST 002: Testing invalid user input when entering the number of players.")
+    void testInvNumPlayers(){
+        Game game = new Game();
+        StringWriter output = new StringWriter();
+        game.setNumPlayers(new Scanner("1\n"), new PrintWriter(output));
+        assertTrue(output.toString().contains("ERROR: Invalid number of players."));
+
+        game = new Game();
+        output = new StringWriter();
+        game.setNumPlayers(new Scanner("6\n"), new PrintWriter(output));
+        assertTrue(output.toString().contains("ERROR: Invalid number of players."));
+    }
 }
