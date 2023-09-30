314be4f1718da23f2ae2fdc9ec1cb96b0b540100
Jake Taylor <jakektaylor@cmail.carleton.ca>
2023-09-30 01:13:30 -0400

    CODE 003: Testing that the user is forced to enter a valid number of players before proceeding.

--- Assignment1/src/main/java/Game.java
+++ Assignment1/src/main/java/Game.java
@@ -9,13 +9,15 @@ public class Game {
     }
 
     public void setNumPlayers(Scanner input, PrintWriter output) {
-        byte result;
-        System.out.println("Please enter the number of players (3-5): ");
-        result = input.nextByte();
-        System.out.println(result);
-        if(result <3 || result > 5) {
-            System.out.println("ERROR: Invalid number of players.");
-            output.println("ERROR: Invalid number of players.");
+        byte result = -1;
+        while (result < 3 || result > 5) {
+            System.out.println("Please enter the number of players (3-5): ");
+            result = input.nextByte();
+            System.out.println(result);
+            if (result < 3 || result > 5) {
+                System.out.println("ERROR: Invalid number of players.");
+                output.println("ERROR: Invalid number of players.");
+            }
         }
         numPlayers =  result;
     }
