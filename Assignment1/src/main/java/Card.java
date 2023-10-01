4e5c7381dad0890ce403e6e23f9aab4e8367c2a1
Jake Taylor <jakektaylor@cmail.carleton.ca>
2023-10-01 16:50:39 -0400

    CODE 010: Testing the correct number of each type of card is present and that all cards are correctly formatted after creating the entire deck.

--- Assignment1/src/main/java/Card.java
+++ Assignment1/src/main/java/Card.java
@@ -1,9 +1,20 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Card {
+    public static final HashSet<String> SUITS = new HashSet<>(Arrays.asList("Sw", "Ar", "So", "De"));
+    private String type;
+    private Byte value;
+    public Card(String type, Byte value) {
+        this.type = type;
+        this.value = value;
+    }
+
     public String getType() {
-        return "";
+        return type;
     }
 
     public Byte getValue() {
-        return -1;
+        return value;
     }
 }
--- Assignment1/src/main/java/Deck.java
+++ Assignment1/src/main/java/Deck.java
@@ -1,44 +1,74 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Deck {
+    private ArrayList<Card> cards;
+    HashMap<String, Integer> cardCounts;         //HashMap to store the number of cards of each type in the Deck.
+
+    public Deck() {
+        cards = new ArrayList<>();
+        cardCounts = new HashMap<>();
+        cardCounts.put("Sw", 0);
+        cardCounts.put("Ar", 0);
+        cardCounts.put("So", 0);
+        cardCounts.put("De", 0);
+        cardCounts.put("Al", 0);
+        cardCounts.put("Me", 0);
+        cardCounts.put("Ap", 0);
+    }
+
     public static Deck createGameDeck() {
-        return new Deck();
+        Deck deck = new Deck();
+        //Create the basic weapon cards.
+        for(String suit:Card.SUITS) {
+            for(int i=0;i<15;i++) deck.addCard(new Card(suit, (byte)(i+1)));
+        }
+        //Create the alchemy cards.
+        for(int i=0;i<15;i++) deck.addCard(new Card("Al", (byte) (i+1)));
+        for(int i=0;i<3;i++)  deck.addCard(new Card("Me", null));
+        for(int i=0;i<2;i++)  deck.addCard(new Card("Ap", null));
+
+        return deck;
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+        cardCounts.put(card.getType(), cardCounts.get(card.getType()) + 1);
     }
 
     public int getNumCards() {
-        return -1;
+        return cards.size();
     }
 
-
     public int getNumSw() {
-        return -1;
+        return cardCounts.get("Sw");
     }
 
     public int getNumAr() {
-        return -1;
+        return cardCounts.get("Ar");
     }
 
     public int getNumSo() {
-        return -1;
+        return cardCounts.get("So");
     }
 
     public int getNumDe() {
-        return -1;
+        return cardCounts.get("De");
     }
 
     public int getNumAl() {
-        return -1;
+        return cardCounts.get("Al");
     }
 
     public int getNumMe() {
-        return -1;
+        return cardCounts.get("Me");
     }
 
     public int getNumAp() {
-        return -1;
+        return cardCounts.get("Ap");
     }
 
     public ArrayList<Card> getCards() {
-        return new ArrayList<>();
+        return cards;
     }
 }
