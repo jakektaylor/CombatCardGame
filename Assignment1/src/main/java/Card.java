203918caf954ee14bd4f81cb06ddf84f2f1bcd46
Jake Taylor <jakektaylor@cmail.carleton.ca>
2023-10-01 22:10:14 -0400

    CODE 011: Test that non-poisoned Basic Weapon cards and Alchemy cards and Merlin cards and Apprentice cards have the correct damage value.

--- Assignment1/src/main/java/Card.java
+++ Assignment1/src/main/java/Card.java
@@ -1,13 +1,25 @@
 import java.util.Arrays;
 import java.util.HashSet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Card {
     public static final HashSet<String> SUITS = new HashSet<>(Arrays.asList("Sw", "Ar", "So", "De"));
     private String type;
     private Byte value;
+    private Byte damage;
     public Card(String type, Byte value) {
         this.type = type;
         this.value = value;
+
+        //Set the correct damage value.
+        if(type.equals("Sw") && (value < 6 || value > 9)) damage =5;
+        else if (type.equals("Ar") && (value < 8 || value > 11)) damage = 5;
+        else if (type.equals("So") && value != 5 && value != 6 && value != 11 && value != 12) damage = 5;
+        else if (type.equals("De") && value != 6 && value != 7 && value != 9 && value != 10) damage = 5;
+        else if(type.equals("Al")) damage = 5;
+        else if (type.equals("Me")) damage = 25;
+        else if (type.equals("Ap")) damage = 5;
     }
 
     public String getType() {
@@ -19,6 +31,6 @@ public class Card {
     }
 
     public Byte getDamage() {
-        return -1;
+        return damage;
     }
 }
