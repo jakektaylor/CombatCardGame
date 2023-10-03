607cbd04ccd60e0a6d157a563a9820a062c89872
Jake Taylor <jakektaylor@cmail.carleton.ca>
2023-10-03 12:22:34 -0400

    U-TEST 026: Testing that if the suit for the melee has been set, if a Player tries to play a basic weapon Card of a different suit, an error message will be displayed and the Card is not played.

--- Assignment1/src/test/java/MeleeTest.java
+++ Assignment1/src/test/java/MeleeTest.java
@@ -47,4 +47,27 @@ class MeleeTest {
         melee.playCard((byte) 0, new Card("Ap", null), new Scanner("\nSo\n50\n14\n"), new PrintWriter(output));
         assertTrue(output.toString().contains("ERROR: Please enter a value between 1 and 15."));
     }
+
+    @Test
+    @DisplayName("U-TEST 026: Testing that if the suit for the melee has been set, if a Player tries to play a basic weapon" +
+            "Card of a different suit, an error message will be displayed and the Card is not played.")
+    void testInvBasic() {
+        Melee melee = new Melee((byte) (3));
+        StringWriter output = new StringWriter();
+        //Set the suit for the Melee to "De".
+        melee.playCard((byte) 0, new Card("De", (byte) 12), new Scanner(""), new PrintWriter(output));
+
+        //Try to play Cards of a different suit after.
+        melee.playCard((byte) 1, new Card("Sw", (byte) 8), new Scanner(""), new PrintWriter(output));
+        melee.playCard((byte) 2, new Card("Ar", (byte) 13), new Scanner(""), new PrintWriter(output));
+
+        //Check that the error message was printed twice.
+        assertEquals(2, output.toString().split
+                (String.format("ERROR: A basic weapon card can only be played if it is of suit %s.", melee.getSuit()),
+                        -1).length - 1);
+
+        //Check that the invalid Cards were not played.
+        assertNull(melee.getPlayed()[1]);
+        assertNull(melee.getPlayed()[2]);
+    }
 }
