00b75ee8f649a786c2631bb2ecc980aca2c57f84
Jake Taylor <jakektaylor@cmail.carleton.ca>
2023-10-02 23:43:16 -0400

    CODE 021: Testing that the first Player playing a basic weapon card sets the Suit for the Melee.

--- Assignment1/src/main/java/Melee.java
+++ Assignment1/src/main/java/Melee.java
@@ -1,9 +1,11 @@
 public class Melee {
 
     private Card[] played;
+    private String suit;
     public Melee(byte numPlayers) {
         played = new Card[numPlayers];
         for(int i=0;i<numPlayers;i++) played[i] = null;
+        suit = null;
     }
 
     public Card[] getPlayed() {
@@ -11,10 +13,21 @@ public class Melee {
     }
 
     public void playCard(byte currPlayer, Card card) {
+        //Check if this is the first Card played.
+        boolean isEmpty = true;
+        for (int i = 0; i < played.length; i++) {
+            if (played[i] != null) {
+                isEmpty = false;
+                break;
+            }
+        }
+
+        //Basic Weapon as first card.
+        if(isEmpty && Card.SUITS.contains(card.getType())) this.suit = card.getType();
         played[currPlayer] = card;
     }
 
     public String getSuit() {
-        return null;
+        return suit;
     }
 }
