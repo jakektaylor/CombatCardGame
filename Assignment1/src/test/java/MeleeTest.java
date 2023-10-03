64a075823f593541956fd8bb33d200c4eb5e0419
Jake Taylor <jakektaylor@cmail.carleton.ca>
2023-10-03 03:18:13 -0400

    U-TEST 024: Testing invalid user input for the suit when the first Card played is a Merlin or Apprentice Card.

--- Assignment1/src/test/java/MeleeTest.java
+++ Assignment1/src/test/java/MeleeTest.java
@@ -19,4 +19,15 @@ class MeleeTest {
         assertEquals("Sw", melee.getSuit());
         assertEquals((byte)9, melee.getPlayed()[0].getValue());
     }
+
+    @Test
+    @DisplayName("U-TEST 024: Testing invalid user input for the suit when the first Card played is a Merlin or Apprentice" +
+            "Card.")
+    void testInvInput() {
+        Melee melee = new Melee((byte) 3);
+        StringWriter output = new StringWriter();
+        melee.playCard((byte) 0, new Card("Ap", null), new Scanner("\nPa\n2\n"), new PrintWriter(output));
+        //Check that an error message is displayed.
+        assertTrue(output.toString().contains(String.format("ERROR: Please enter a valid suit from %s.", Card.SUITS)));
+    }
 }
