c6d4760a701a20c4d00a662bbfb04131e96aac4c
Jake Taylor <jakektaylor@cmail.carleton.ca>
2023-10-04 21:12:32 -0400

    REFAC 015: Adding a TestSuite to run all U-TESTS at once.

---
+++ Assignment1/src/test/java/UnitTestSuite.java
@@ -0,0 +1,9 @@
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("Test suite for running all of the Unit tests from Part 1.")
+@SelectClasses({DeckTest.class, MeleeTest.class, GameTest.class})
+public class UnitTestSuite {
+}
